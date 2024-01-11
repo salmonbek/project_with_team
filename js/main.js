10e1d605a7fc766653b0220085ed78b654b647b2
“salmonbek” <salmonlearner34@gmail.com>
2024-01-11 21:49:08 +0500

    sasdsadsadsadfasdsa

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
-// function openNavbar() {
-//   document.getElementById("navbar-responsive").style.left = "0";
-//   // document.getElementById("navbar-responsive").style.top = "0";
-// }
-// function closeNavbar() {
-//   document.getElementById("navbar-responsive").style.left = "-100%";
-//   // document.getElementById("navbar-responsive").style.top = "-100%";
-// }
+function openNavbar() {
+  document.getElementById("navbar-responsive").style.left = "0";
+  // document.getElementById("navbar-responsive").style.top = "0";
+}
+function closeNavbar() {
+  document.getElementById("navbar-responsive").style.left = "-100%";
+  // document.getElementById("navbar-responsive").style.top = "-100%";
+}
 
-// document.getElementById("navbar-open").addEventListener("click", openNavbar);
-// document.getElementById("navbar-close").addEventListener("click", closeNavbar);
+document.getElementById("navbar-open").addEventListener("click", openNavbar);
+document.getElementById("navbar-close").addEventListener("click", closeNavbar);
 
-// window.addEventListener("scroll", function () {
-//   toggleBacktop();
-// });
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
 
-// let backtop = document.getElementById("backtop");
+let backtop = document.getElementById("backtop");
 
-// function toggleBacktop() {
-//   if (
-//     document.body.scrollTop > 300 ||
-//     document.documentElement.scrollTop > 300
-//   ) {
-//     backtop.classList.add("backtop-show");
-//   } else {
-//     backtop.classList.remove("backtop-show");
-//   }
-// }
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    backtop.classList.add("backtop-show");
+  } else {
+    backtop.classList.remove("backtop-show");
+  }
+}
 
-// $(document).ready(function () {
-//   $(".owl-carousel").owlCarousel();
-// });
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel();
+});
